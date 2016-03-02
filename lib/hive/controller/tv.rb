4ba17978d3e807d4ffe1b92975a25afd1ddd67e7
Joe Haig <joe.haig@bbc.co.uk>
2016-03-02 09:47:21 +0000

    Get devices from Hive Mind

--- lib/hive/controller/tv.rb
+++ lib/hive/controller/tv.rb
@@ -19,19 +19,20 @@ module Hive
           mm_devices = []
         end
         Hive.logger.debug("Devices: #{mm_devices}")
+        mm_devices
 
-        hive_details = Hive.devicedb('Hive').find(Hive.id)
-        if hive_details.key?('devices')
-          hive_details['devices'].select { |d| d['device_type'] == 'tv' }.collect do |device|
-            Hive.logger.debug("Found TV #{device}")
-            device['queues'] = device['device_queues'].collect do |queue_details|
-              queue_details['name']
-            end
-            self.create_device(device)
-          end
-        else
-          raise DeviceDetectionFailed
-        end
+        #hive_details = Hive.devicedb('Hive').find(Hive.id)
+        #if hive_details.key?('devices')
+        #  hive_details['devices'].select { |d| d['device_type'] == 'tv' }.collect do |device|
+        #    Hive.logger.debug("Found TV #{device}")
+        #    device['queues'] = device['device_queues'].collect do |queue_details|
+        #      queue_details['name']
+        #    end
+        #    self.create_device(device)
+        #  end
+        #else
+        #  raise DeviceDetectionFailed
+        #end
       end
     end
   end
