e130bc17d5a5dc454e213371060cea14de5de5c3
Joe Haig <joe.haig@bbc.co.uk>
2016-03-21 11:12:42 +0000

    Move queue generation into worker

--- hive-runner-tv.gemspec
+++ hive-runner-tv.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |s|
   s.name        = 'hive-runner-tv'
-  s.version     = '0.1.1'
+  s.version     = '0.1.2'
   s.date        = Time.now.strftime("%Y-%m-%d")
   s.summary     = 'Hive Runner TV'
   s.description = 'The TV controller module for Hive Runner'
--- lib/hive/device/tv.rb
+++ lib/hive/device/tv.rb
@@ -6,6 +6,9 @@ module Hive
     class Tv < Device
       def initialize(config)
         @identity = config['id']
+
+        # TODO This can be removed when DeviceDB is no longer being used
+        config['queues'] = [ "#{config['brand'].downcase.gsub(/\s/, '_')}-#{config['model'].downcase.gsub(/\s/, '_')}-test" ]
         super
       end
     end
