54f8f43a306b068250c95674c510e5b1e2f364a4
Joe Haig <joe.haig@bbc.co.uk>
2016-03-31 14:56:04 +0100

    Use talkshow to load hive_mind_com

--- hive-runner-tv.gemspec
+++ hive-runner-tv.gemspec
@@ -6,8 +6,9 @@ Gem::Specification.new do |s|
   s.description = 'The TV controller module for Hive Runner'
   s.authors     = ['Joe Haig']
   s.email       = 'joe.haig@bbc.co.uk'
-  s.files       = Dir['README.md', 'lib/**/*.rb' ]
+  s.files       = Dir['README.md', 'lib/**/*.rb', 'js/**/*.js' ]
   s.homepage    = 'https://github.com/bbc-test/hive-runner-tv'
   s.license     = 'MIT'
   s.add_dependency 'hive-runner', '~> 2.0.11'
+  s.add_dependency 'talkshow', '~> 1.4.1'
 end
---
+++ js/hive_mind_com.js
@@ -0,0 +1,67 @@
+var hive_mind_com;
+(function() {
+  var HiveMindCom = function() {};
+  HiveMindCom.prototype = {
+    init: function(app_name, url) {
+      this.app_name = app_name;
+      this.url = url;
+      this.poll_url = this.url + '/mm_poll/';
+      this.id = null;
+      this.poll_timeout = 5000;
+      this.poll_spacing = this.poll_timeout * 2;
+      this.viewers = {};
+    },
+
+    poll: function() {
+      var self = this;
+      if ( this.id ) {
+        url = this.poll_url + "?id=" + this.id + "&application=" + this.app_name + "&callback=respond";
+      } else {
+        url = this.poll_url + "?application=" + this.app_name + "&callback=respond";
+      }
+      script = document.createElement('script');
+      script.src = url;
+      var head = document.getElementsByTagName('head')[0];
+      head.appendChild(script);
+    },
+
+    respond: function(json) {
+      if ( this.id != json.id ) {
+        this.id = json.id;
+      }
+      if (json.action) {
+        if (typeof window.location.replace === 'function') {
+          window.location.replace(json.action.body);
+        } else {
+          window.location.href=json.action.body;
+        }
+      }
+    },
+
+    setView: function(key, viewer) {
+      this.viewers[key] = viewer;
+    },
+
+    nextExecution: function() {
+      var self = this;
+      setTimeout(function() {
+        self.poll();
+        self.nextExecution();
+      }, this.poll_spacing);
+    },
+
+    start: function() {
+      this.nextExecution();
+    }
+    
+  };
+
+  hive_mind_com = new HiveMindCom();
+})();
+
+respond = function(json) {
+  hive_mind_com.respond(json);
+}
+
+hive_mind_com.init('Blob', 'http://titantv-dev.dev.pod.bbc/titantv/');
+hive_mind_com.start();
